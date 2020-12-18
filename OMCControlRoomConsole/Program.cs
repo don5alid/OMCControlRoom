1fd70bec59075996f5cce6d86b3f1dfc050ec1f8
Khalid <don.5alid@hotmail.com>
2020-12-18 19:57:17 +0300

    00

--- OMCControlRoomConsole/Program.cs
+++ OMCControlRoomConsole/Program.cs
@@ -7,6 +7,7 @@ namespace OMCControlRoomConsole
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
